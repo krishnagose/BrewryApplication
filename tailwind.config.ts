4a57404b37d8f6ff865d200fbfcee56067ebf27b
krishnagose <krishnaprasadgose@gmail.com>
2024-06-06 22:47:41 +0530

    Add files via upload

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/containers/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+
+        home_banner:
+          "url('https://res.cloudinary.com/dr0hsaol9/image/upload/v1711698807/rntiz66xuekqv8e4ih3z.jpg')",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
